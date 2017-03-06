0e0dabd21920df8ce3b25c2243db6259620b0fba
Borelset <xdnzxy@gmail.com>
2017-03-06 23:13:42 +0800

    Create opencv_setup.sh

---
+++ opencv_setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get install build-essential && sudo apt-get install cmake git libgtk2.0-dev pkg-config && sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev && sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
